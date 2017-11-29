d9b2627a0a738e7ce42704fd2816dac585b32b46
Dylan Hobbs <dhobbs@tcd.ie>
2017-11-29 00:13:52 +0000

    Successful parse of one file

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,38 @@
 module Lib
-    ( someFunc
-    ) where
+    where
 
+import System.Environment
+import Types
+import Data.List.Split
+
+-- Main entry point Grab the IO Programs here
+-- Lets get one file read for now
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+      programPath <- getArgs
+      contents <- readFile (head programPath)
+      let line = lines contents
+      --let listOfPaths = splitOn " " programPaths
+      let programs = parseProgram line
+      print programs
+
+
+dropLast :: String -> String
+dropLast []     = []
+dropLast [x]    = []
+dropLast (x:xs) = x : dropLast xs
+
+parseProgram :: [String] -> Program
+parseProgram lines = do
+      --contents <- readFile file
+      --let line = lines contents
+      --[(addr, f1, f2)] <- map splitOn line
+      let x = map (splitOn " ") lines
+      let programs = createProgram x
+      programs
+
+
+
+createProgram :: [[String]] -> Program
+createProgram ([x,y,z]:rest) = newRedcode x (Just (dropLast y)) (Just z) : createProgram rest
+createProgram _ = []
--- src/Types.hs
+++ src/Types.hs
@@ -2,26 +2,28 @@ module Types where
 
 type Program = [Redcode]
 
+{-|
+  Redcode data type hold a single redcode instruction.
+-}
 data Redcode = Redcode {
                 instruction  :: Instruction
               , fieldA       :: Maybe Field
               , fieldB       :: Maybe Field
               }
               deriving (Read, Show)
-newRedcode :: String -> Maybe String -> Maybe String -> Maybe Redcode
-newRedcode instruction fieldA fieldB = do
-  fieldAA   <-
-    case fieldA of
-      (Just fieldA) -> Just $ newField fieldA
-      Nothing -> Nothing
-  fieldBB   <-
-    case fieldB of
-      (Just fieldB) -> Just $ newField fieldB
-      Nothing -> Nothing
-  return $ Redcode (read instruction :: Instruction) fieldAA fieldBB
+newRedcode :: String -> Maybe String -> Maybe String -> Redcode
+newRedcode instruction fieldA fieldB = Redcode (read instruction :: Instruction) fieldAA fieldBB
+            where
+                fieldAA  =
+                  case fieldA of
+                    (Just fieldA) -> newField fieldA
+                    Nothing -> Nothing
+                fieldBB   =
+                  case fieldB of
+                    (Just fieldB) -> newField fieldB
+                    Nothing -> Nothing
 
-
-data Instruction = DAT | ADD | SUB | JMP | JMZ | JMN | DJN | CMP | SPL
+data Instruction = DAT | ADD | SUB | JMP | JMZ | JMN | DJN | CMP | SPL | MOV
       deriving (Read, Show)
 
 data Field = Field {
@@ -35,6 +37,6 @@ newField input = do
   let (addrMode, address) = case optionalAddrMode of
                               '@' -> ('@', drop 1 input)
                               '#' -> ('#', drop 1 input)
-                              _   -> ('$', read input)
+                              _   -> ('$', input)
   return $ Field addrMode (read address :: Int)
 
